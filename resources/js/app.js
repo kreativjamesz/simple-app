d1a6c973bd5d9930fd8bb5056247f05918fd43b6
kreativjamesz <kreativjames@gmail.com>
2020-04-16 09:31:00 +0800

    secondary commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
+import Vue from 'vue';
+//window.Vue = require('vue');
+import VueRouter from 'vue-router';
 require('./bootstrap');
+import routes from './routes';
+
+
+Vue.use(VueRouter);
+
+
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+let app = new Vue({
+    el: '#app',
+    router: new VueRouter(routes)
+});
