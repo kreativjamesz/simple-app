46997007347f0175eaac04a82b5d218739674374
kreativjamesz <kreativjames@gmail.com>
2020-04-16 16:04:49 +0800

    [Dev Branch] Adds Url, Created Date  Accessors

--- app/Question.php
+++ app/Question.php
@@ -10,12 +10,31 @@ class Question extends Model
     protected $fillable = ['title','body'];
     
     // We may now be able to get the User created the question.
-    public function user() {
+    public function user() 
+    {
         return $this->belongsTo(User::class);
     }
 
-    public function setTitleAttribute($value) {
+    public function setTitleAttribute($value)
+    {
         $this->attributes['title'] = $value;
         $this->attributes['slug'] = Str::slug($value);
     }
+
+    public function getUrlAttribute()
+    {
+        return route('questions.show', $this->id);
+    }
+
+    public function getCreatedDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
+    }
+
+    public function getNameAttribute()
+    {
+
+    }
+
+
 }
