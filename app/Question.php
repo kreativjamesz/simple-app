4ae22358e41f688cd5fc6db28710ac0cb61fca22
kreativjamesz <kreativjames@gmail.com>
2020-04-16 10:25:34 +0800

    [Dev Branch] Populates User and Questions table using Model Factory

--- app/Question.php
+++ app/Question.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Question extends Model
 {
@@ -11,5 +12,10 @@ class Question extends Model
     // We may now be able to get the User created the question.
     public function user() {
         return $this->belongsTo(User::class);
+    }
+
+    public function setTitleAttribute($value) {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
     }
 }
--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -7,6 +7,10 @@ use Faker\Generator as Faker;
 
 $factory->define(Question::class, function (Faker $faker) {
     return [
-        //
+        'title' => rtrim($faker->sentence(rand(5,10)),"."),
+        'body' => $faker->paragraphs(rand(3,7), true),
+        'views' => rand(0,5),
+        'answers' => rand(0,10),
+        'ratings' => rand(-5,10),
     ];
 });
